92cc531db08ada0ea56dfd50304a068e4e6f402a
Brosda <abrosda@ualberta.ca>
2017-10-05 20:13:01 -0600

    First draft of profile page
    Static images not centered.

--- client/index.js
+++ client/index.js
@@ -1,4 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Hello, World!</h1>, document.getElementById('container'));
+var DATA = {    
+    name: 'Frog',
+    imgURL: 'https://ichef-1.bbci.co.uk/news/624/cpsprodpb/AA04/production/_87842534_bbc_biswas_7.jpg'
+}
+var Profile = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<h3>{this.props.name}</h3>
+				<img src={this.props.imgURL}/>
+			</div>
+		);
+	}
+});
+ReactDOM.render(<Profile />, document.getElementById('content'));
+
