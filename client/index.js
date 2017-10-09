0b2dfc2978e0df0cc6fa8bf94494c74d18e17dc2
Brosda <abrosda@ualberta.ca>
2017-10-09 17:03:08 -0600

    New data list added below image

--- client/index.js
+++ client/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
 var DATA = {    
     name: 'Frog',
-    imgURL: 'https://ichef-1.bbci.co.uk/news/624/cpsprodpb/AA04/production/_87842534_bbc_biswas_7.jpg'
+    imgURL: 'https://ichef-1.bbci.co.uk/news/624/cpsprodpb/AA04/production/_87842534_bbc_biswas_7.jpg',
+    datalist: ['Structure', 'wavelength', 'Factor', 'Location']
 }
+
+var App = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<Profile
+					name={this.props.profileData.name}
+					imgURL={this.props.profileData.imgURL}/>
+				<Data
+					dataList={this,props.profileData.dataList} />
+			</div>
+		);
+	}
+});
+
 var Profile = React.createClass({
 	render: function(){
 		return (
@@ -15,5 +32,23 @@ var Profile = React.createClass({
 		);
 	}
 });
-ReactDOM.render(<Profile />, document.getElementById('content'));
+
+var Data = React.createClass({
+	render:function(){
+		var data = this.props.dataList.map(function(data,index){
+			return (<li key={index}>{data}</li>);
+	});
+
+	return (
+		<div>
+			<ul>
+				{data}
+			</ul>
+		</div>
+		);
+	}
+});
+
+
+ReactDOM.render(<App profileData={DATA} />, document.getElementById('content'));
 
