280a93bac1ce56fa049bfd5c89438723d1de0b67
Brosda <abrosda@ualberta.ca>
2017-10-19 21:50:48 -0600

    picture actions
    makes action file for pictures

---
+++ client/actions/pictureActions.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+import { URL_PREFIX } from '../constants';
+
+export function fetchPicture(id) {
+  return function(dispatch) {
+    dispatch({type: "FETCH_PICTURE"});
+    axios.get(URL_PREFIX + "/api/species/" + id)
+    .then((response) => {
+      dispatch({type: "FETCH_PICTURE_FULFILLED", payload: response.data})
+    })
+    .catch((err) => {
+      dispatch({type: "FETCH_PICTURE_REJECTED", payload: err})
+    })
+  }
+}
