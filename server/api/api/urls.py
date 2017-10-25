a6872286576ef2aea84dae81317e303634bd5421
dhaberst <daniel.haberstock1@gmail.com>
2017-10-24 22:16:52 -0600

    Added backend code for landing page pictures :turtle:.

--- server/api/api/urls.py
+++ server/api/api/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     url(r'^species/(?P<pk>[0-9]+)/$', SpeciesDetailAPIView.as_view(), name='species-detail'),
     url(r'^pictures/all/$', PictureListAPIView.as_view(), name='pictures-all'),
-    url(r'^pictures/random/$', RandomPictureListAPIView.as_view(), name='pictures-random'),
+    url(r'^pictures/random/$', RandomLandingPictureListAPIView.as_view(), name='pictures-random'),
     url(r'^taxonomy/all/$', TaxonomyListAPIView.as_view(), name='taxonomy-all'),
     url(r'^colour/all/$', ColourListAPIView.as_view(), name='coloury-all'),
 ]
