31dedaf0e1563cd7fe5c199e70ffeef91518bce3
Sean Baergen <sbaergen@ualberta.ca>
2017-10-28 11:50:42 -0600

    Remove SECURE_SSL_REDIRECT setting :horse:

--- server/StructuralColour/settings.py
+++ server/StructuralColour/settings.py
@@ -27,7 +27,7 @@ with open('/etc/secret_key.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '162.246.156.147']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', '162.246.156.147']
 
 
 # Application definition
@@ -146,4 +146,3 @@ X_FRAME_OPTIONS = 'DENY'
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SESSION_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
-SECURE_SSL_REDIRECT = True
