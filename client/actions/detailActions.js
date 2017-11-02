322f3a531d08982df16d8a4f005cd97784eacd5d
iali17 <iali1@ualberta.ca>
2017-11-02 09:38:30 -0600

    Added the dispatch to fetch Taxonomy :snake:

--- client/actions/detailActions.js
+++ client/actions/detailActions.js
@@ -14,3 +14,16 @@ export function fetchDetail(id) {
     })
   }
 }
+
+export function fetchTax() {
+  return function(dispatch) {
+    dispatch({type: "FETCH_DETAIL"});
+    axios.get(URL_PREFIX + "/api/taxonomy/all")
+    .then((response) => {
+      dispatch({type: "FETCH_DETAIL_FULFILLED", payload: response.data})
+    })
+    .catch((err) => {
+      dispatch({type: "FETCH_DETAIL_REJECTED", payload: err})
+    })
+  }
+}
