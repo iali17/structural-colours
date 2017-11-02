7c2bbd47db3f2b3f6ef6b45a33bff405300820b6
Sean <sbaergen@ualberta.ca>
2017-11-02 20:13:02 +0000

    Updated mod_wsgi to 3.4 :frog:

--- server/StructuralColour/settings.py
+++ server/StructuralColour/settings.py
@@ -25,7 +25,7 @@ with open('/etc/secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1', '162.246.156.147']
 
