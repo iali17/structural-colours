a938b998bde34f3b7c8fce9871c9408c2e3f30b8
dhaberst <daniel.haberstock1@gmail.com>
2017-11-08 10:04:11 -0700

    Now filters pictures correctly based on colour or species :cat2:.

--- server/api/api/views.py
+++ server/api/api/views.py
@@ -31,11 +31,23 @@ class PictureListAPIView(ListAPIView):
 
         # TODO: This is where we will do the advanced search.
         if colour_param is not None and species_param is not None:
-            queryset = Picture.objects.all().order_by('id')
-        else if colour_param is not None:
-            pass
-        else if species_param is not None:
-            pass
+            # One liner to get all the species from SpeciesColour using colour_param and species_param
+            species_list = SpeciesColour.objects.filter(colour__colour=colour_param, species__common_name__icontains=species_param).values_list('species')
+
+            # Set queryset with species_list list
+            queryset = Picture.objects.filter(species__in=species_list).order_by('id')
+        elif colour_param is not None:
+            # Get all the species with colour = colour_param
+            species_list = SpeciesColour.objects.filter(colour__colour=colour_param).values_list('species')
+
+            ## Set queryset with species_list list
+            queryset = Picture.objects.filter(species__in=species_list).order_by('id')
+        elif species_param is not None:
+            # Get all the species with colour = colour_param
+            species_list = SpeciesColour.objects.filter(species__common_name__icontains=species_param).values_list('species')
+
+            # Set queryset with species_list list
+            queryset = Picture.objects.filter(species__in=species_list).order_by('id')
 
         return queryset
 
