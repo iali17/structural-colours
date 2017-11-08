0318114977a10b67e8f794aa4b59dbd752d6ca56
dhaberst <daniel.haberstock1@gmail.com>
2017-11-08 10:11:49 -0700

    Changed some comments and formatting :pig:.

--- server/api/api/views.py
+++ server/api/api/views.py
@@ -29,24 +29,20 @@ class PictureListAPIView(ListAPIView):
         colour_param = self.request.query_params.get('colour')
         species_param = self.request.query_params.get('species')
 
-        # TODO: This is where we will do the advanced search.
         if colour_param is not None and species_param is not None:
             # One liner to get all the species from SpeciesColour using colour_param and species_param
             species_list = SpeciesColour.objects.filter(colour__colour=colour_param, species__common_name__icontains=species_param).values_list('species')
-
-            # Set queryset with species_list list
+            # Set queryset with species_list
             queryset = Picture.objects.filter(species__in=species_list).order_by('id')
         elif colour_param is not None:
             # Get all the species with colour = colour_param
             species_list = SpeciesColour.objects.filter(colour__colour=colour_param).values_list('species')
-
-            ## Set queryset with species_list list
+            ## Set queryset with species_list
             queryset = Picture.objects.filter(species__in=species_list).order_by('id')
         elif species_param is not None:
-            # Get all the species with colour = colour_param
+            # Get all the species with species = species_param
             species_list = SpeciesColour.objects.filter(species__common_name__icontains=species_param).values_list('species')
-
-            # Set queryset with species_list list
+            # Set queryset with species_list
             queryset = Picture.objects.filter(species__in=species_list).order_by('id')
 
         return queryset
