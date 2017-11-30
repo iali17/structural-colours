2f87d59c41de2520b10258e76c32c4f69b10f9d2
dhaberst <daniel.haberstock1@gmail.com>
2017-11-30 14:42:34 -0700

    Prettied up pictures :rainbow:.

--- client/components/LandingPic.js
+++ client/components/LandingPic.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class MainPic extends Component {
+export default class LandingPic extends Component {
   constructor(props) {
     super(props);
   }
--- client/components/MainView.js
+++ client/components/MainView.js
@@ -65,7 +65,7 @@ class MainView extends Component {
   }
 
   getNextPictures() {
-    if (this.props.pictures.next) {
+    if (this.props.pictures.next && !this.props.fetching) {
       this.props.dispatch(fetchNextPictures(this.props.pictures.next))
     }
   }
@@ -90,10 +90,12 @@ class MainView extends Component {
       return (
         <div>
           <div className={classes.root}>
-            <GridList cellHeight={'auto'} className={classes.gridList} cols = {'auto'} spacing = {0}>
+            <GridList cellHeight={'auto'} className={classes.gridList} cols={'auto'} spacing={0}>
               {this.props.pictures.results.map((picture, index) => (
                 <GridListTile key={index} >
-                  <MainPic pic={picture} getProfile={this.props.getProfile}/>
+                  <div style={{width:'100px', height: '100px'}}>
+                    <MainPic pic={picture} getProfile={this.props.getProfile}/>
+                  </div>
                 </GridListTile>
               ))}
             </GridList>
@@ -116,4 +118,5 @@ class MainView extends Component {
     }
   }
 }
+
 export default withStyles(styles)(MainView);
