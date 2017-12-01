3ccb538b9a8764d7d6606d7b66a3d742eb86be63
iali17 <iali1@ualberta.ca>
2017-12-01 00:18:36 -0700

    Testing to try to get a store'd component to work :dancer:

--- client/tests/icicle.test.js
+++ client/tests/icicle.test.js
@@ -11,12 +11,14 @@ import { createMockDispatch } from 'redux-test-utils';
 import thunk from 'redux-thunk'
 import { fetchTax, } from '../actions/taxonomyActions'
 import reducer from '../reducers/icicleViewReducer'
+import promise from 'redux-promise-middleware';
+import { applyMiddleware } from 'redux';
 
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const middlewares = [thunk]
-const mockStore = configureMockStore(middlewares)
+const middlewares = applyMiddleware(promise(), thunk)
+const mockStore = configureMockStore(reducer, middlewares)
 
 
 describe('Test the actions', () => {
@@ -26,24 +28,29 @@ describe('Test the actions', () => {
 	})
 
 	it('Tests the fetch taxonomy action', () => {
-		fetchMock
-      	.getOnce('/todos', { body: { payload: ['do something'] }, headers: { 'content-type': 'application/json' } })
 		
 		const expectedActions = [
 	      { type: "FETCH_TAXONOMY" },
-	      { type: "FETCH_TAXONOMY_FULFILLED", payload: ["data?"]}
+	      { type: "FETCH_TAXONOMY_FULFILLED", payload: undefined }
       	]
-      	const store = mockStore({payload: []})
-
-      	console.log("what??", store.dispatch({type: "FETCH_TAXONOMY"}));
+      	const store = mockStore({})
 
       	return store.dispatch(fetchTax()).then(() => {
+      		console.log(store.getState())
       		expect(store.getActions()).to.equal(expectedActions)
       	})
 	})
+	// it('should test the fetchtax action', () => {
+	// 	const expectedAction = {
+	// 		type: "FETCH_TAXONOMY",
+	// 		payload: undefined
+	// 	}
+	// 	expect(fetchTax()).to.equal(expectedAction)
+	// })
+
 })
 
-describe('test the reducers for icicle', () => {
+describe('Reducers for Icicle', () => {
 	it('should return to intial state', () => {
 		expect(reducer(undefined, {})).to.deep.equal(
 		{
@@ -93,6 +100,33 @@ describe('test the reducers for icicle', () => {
 })
 
 
+describe('icicle', () => {
+	it('should render itself PLEASE', () => {
+		const store = mockStore({icicleView: {
+			taxonomy: {}, Tfetching: false, Tfetched: false
+			}
+		});
+
+		const dispatchMock = createMockDispatch();
+	    const action = {
+	      type: 'FETCH_TAXONOMY',
+	    };
+	    dispatchMock.dispatch(action);
+
+		const wrapper = mount(
+   		<Icicle dispatch={dispatchMock} store={store}
+   		/>);
+
+   		expect(wrapper.find("div")).to.have.length(1);
+	})
+})
+
+describe('icicle dispatch', () => {
+	it('fetchtax', () => {
+		const state = 'state';
+	    
+	})
+})
 
 // const shallowWithStore = (component, store) => {
 //   const context = {
@@ -104,58 +138,59 @@ describe('test the reducers for icicle', () => {
 
 // describe('<Icicle />', () => {
 // 	it('should have 1 svg element', () => {
-// 		const state = {
-// 			taxonomy: {},
-// 		  	Tfetching: false,
-// 			Tfetched: false,
-// 			phylum: {},
-// 			Pfetching: false,
-// 			Pfetched: false,
-// 			order: {},
-// 			Ofetching: false,
-// 			Ofetched: false,
-// 			family: {},
-// 			Ffetching: false,
-// 			Ffetched: false,
-// 			species: {},
-// 			Sfetching: false,
-// 			Sfetched: false,
-// 		}
-// 		const action = {
-// 			switch (action.type) {
-// 		    case "FETCH_TAXONOMY": {
-// 		      return {...state, Tfetching: true}
-// 		    }
-// 		    case "FETCH_TAXONOMY_REJECTED": {
-// 		      return {...state, Tfetching: false, error: action.payload}
-// 		    }
-// 		    case "FETCH_TAXONOMY_FULFILLED": {
-// 		      return {
-// 		        ...state,
-// 		        Tfetching: false,
-// 		        Tfetched: true,
-// 		        taxonomy: action.payload,
-// 		      }
-// 		    }
-// 		    case "FETCH_PHYLUM": {
-// 		      return {...state, Pfetching: true}
-// 		    }
-// 		    case "FETCH_PHYLUM_REJECTED": {
-// 		      return {...state, Pfetching: false, error: action.payload}
-// 		    }
-// 		}
-// 		const store = createMockStore(state);
+// 		// const state = {
+// 		// 	taxonomy: {},
+// 		//   	Tfetching: false,
+// 		// 	Tfetched: false,
+// 		// 	phylum: {},
+// 		// 	Pfetching: false,
+// 		// 	Pfetched: false,
+// 		// 	order: {},
+// 		// 	Ofetching: false,
+// 		// 	Ofetched: false,
+// 		// 	family: {},
+// 		// 	Ffetching: false,
+// 		// 	Ffetched: false,
+// 		// 	species: {},
+// 		// 	Sfetching: false,
+// 		// 	Sfetched: false,
+// 		// }
+// 		// const action = {
+// 		// 	switch (action.type) {
+// 		//     case "FETCH_TAXONOMY": {
+// 		//       return {...state, Tfetching: true}
+// 		//     }
+// 		//     case "FETCH_TAXONOMY_REJECTED": {
+// 		//       return {...state, Tfetching: false, error: action.payload}
+// 		//     }
+// 		//     case "FETCH_TAXONOMY_FULFILLED": {
+// 		//       return {
+// 		//         ...state,
+// 		//         Tfetching: false,
+// 		//         Tfetched: true,
+// 		//         taxonomy: action.payload,
+// 		//       }
+// 		//     }
+// 		//     case "FETCH_PHYLUM": {
+// 		//       return {...state, Pfetching: true}
+// 		//     }
+// 		//     case "FETCH_PHYLUM_REJECTED": {
+// 		//       return {...state, Pfetching: false, error: action.payload}
+// 		//     }
+// 		// }
+// 		//const store = createMockStore(state);
 // 		//const dispatchMock = createMockDispatch();
-// 		store.dispatch(action)
+// 		//store.dispatch(action)
 // 		//expect(component.contains(<svg />).to.equal(true));
 // 		//console.log(dispatchMock.isActionTypeDispatched(action), "action", action, "store",store.getActions());
 
-// 		const component = shallowWithStore(<Icicle />, store);
+// 		const component = shallow(Icicle);
+// 		console.log(component)
 
 // 		//expect(store.getAction(action.type)).to.equal(action);
 // 		//expect(store.isActionDispatched(action)).to.equal(true);
 // 	    //expect(store.isActionTypeDispatched(action.type)).to.equal(true);
 // 	    //expect(store.getState()).to.equal(state);
-// 	    expect(component).to.be.a('object');
+// 	    //expect(component).to.be.a('object');
 // 	})
 // });
