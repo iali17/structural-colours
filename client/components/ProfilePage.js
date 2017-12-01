6005622b6e8d2737a1ea4e856de453fb156c21cc
dhaberst <daniel.haberstock1@gmail.com>
2017-12-01 15:58:14 -0700

    Added some style plus error fixes :goat:.

--- client/components/ProfilePage.js
+++ client/components/ProfilePage.js
@@ -208,13 +208,13 @@ export default class ProfilePage extends Component {
 							<Typography type="headline" component="h3">
 								{this.props.detail.family},
 								{this.props.detail.species}
-								<p>
-  								<img src = {this.props.detail.sillouette}/>
-  								<img src = {this.props.picture.picture}/>
-								</p>
-      						</Typography>
-      					</CardContent>
-						<ul style = {{listStyleType: 'none'}}>
+      				</Typography>
+              <p>
+                <img src = {this.props.detail.sillouette}/>
+                <img src = {this.props.picture.picture}/>
+              </p>
+      			</CardContent>
+						<ul style = {{listStyleType: 'none', fontSize: "Larger"}} >
 							<CardContent>
 								 <Typography>
 										{data}
