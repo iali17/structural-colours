f1c5830028a7713ac4cd635f7a08dd1fc4b44e46
iali17 <iali1@ualberta.ca>
2017-11-30 22:04:57 -0700

    Color bar tests :de:
    This is for non store components

---
+++ client/tests/colorBar.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
+
+import ColorBar from '../components/ColorBar';
+
+describe('<ColorBar />', () => {
+  	it('renders a svg components', () => {
+	    const wrapper = shallow(<ColorBar />);
+	    expect(wrapper.find("svg")).to.have.length(1);
+  	});
+
+ 	it('All 7 colors are there', () => {
+	    const onButtonClick = sinon.spy();
+	    const wrapper = shallow(<ColorBar />);
+	    expect(wrapper.find("circle")).to.have.length(7);
+  	});
+
+ 	it('Clicks on one of the circles', () => {
+	    const onButtonClick = sinon.spy();
+	    const wrapper = mount((
+	      <ColorBar updateColour= {onButtonClick} />
+	    ));
+
+	    wrapper.find('circle').at(0).simulate('click');
+	    expect(onButtonClick).to.have.property('callCount', 1);
+  	});
+});
