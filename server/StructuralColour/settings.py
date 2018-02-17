0bc9a362b4867007da14192e5509d35cb714ec4b
Sean Baergen <sbaergen@ualberta.ca>
2018-02-17 14:20:02 -0500

    Update media_root
    Makes uploaded files saved to correct directory

--- server/StructuralColour/settings.py
+++ server/StructuralColour/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'media'),)
 STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../static', 'media')
 
 if not DEBUG:
     STATIC_URL = '/static/'
