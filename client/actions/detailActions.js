88d7ad8482bc432728c9d3850435395a4995e60d
dhaberst <daniel.haberstock1@gmail.com>
2018-02-27 12:18:49 -0700

    Fixed the actions and reducer for articles :rainbow:.

--- client/actions/detailActions.js
+++ client/actions/detailActions.js
@@ -14,30 +14,12 @@ export function fetchDetail(id) {
   }
 }
 
-export function fetchAuthor(id) {
-  return function(dispatch) {
-    dispatch({type: "FETCH_AUTHOR"});
-    axios.get(URL_PREFIX + "/api/articles/all", {
-      params: {
-        species: id
-      }
-    })
-    .then((response) => {
-      dispatch({type: "FETCH_AUTHOR_FULFILLED", payload: response.data})
-    })
-    .catch((err) => {
-      dispatch({type: "FETCH_AUTHOR_REJECTED", payload: err})
-    })
-  }
-}
-
-export function fetchArticle([author_id, species_id]) {
+export function fetchArticle(speciesId) {
   return function(dispatch) {
    dispatch({type: "FETCH_ARTICLE"});
-    axios.get(URL_PREFIX + "/api/articles/author/", {
+    axios.get(URL_PREFIX + "/api/articles/all/", {
       params: {
-        author: author_id,
-        species: species_id
+        species: speciesId
       }
     })
     .then((response) => {
